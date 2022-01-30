e7229ddb0ddb21c9bf244ea3dbda77ad67949ce6
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2022-01-31 00:29:39 +0700

    up

--- js/main.js
+++ js/main.js
@@ -15,24 +15,20 @@ const months = [
 
 const songs = [
   {
-    id: 1,
-    name: "Ái Nộ",
-    singer: "Masew, Khôi Vũ",
-    path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1021/AiNo1-MasewKhoiVu-7078913.mp3?st=ngcoKLRyRorVu8KqUeS1wg&e=1638762705",
+    name: "Con Bướm Xuân",
+    singer: "Hồ Quang Hiếu",
+    path: "https://aredir.nixcdn.com/NhacCuaTui200/NgayTetQueEm-HoNgocHa-VMusic-Min_3kpfq.mp3?st=4U7CO_i2F036MqttZ41Z2w&e=1643648359",
     image:
-      "https://avatar-nct.nixcdn.com/song/2021/08/30/2/1/a/e/1630316309035.jpg",
+      "https://avatar-ex-swe.nixcdn.com/song/2020/01/20/3/e/d/c/1579487864483.jpg",
   },
   {
-    id: 2,
-    name: "Bước Qua Nhau",
-    singer: "Vũ",
+    name: "Ngày Tết Quê Em",
+    singer: "Hồ Ngọc Hà",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1024/BuocQuaNhau-Vu-7120388.mp3?st=I9W59X1Odyi9QRGTehWfHg&e=1638708688",
     image:
-      "https://avatar-nct.nixcdn.com/song/2021/11/19/6/d/9/1/1637317177185.jpg",
+      "https://avatar-ex-swe.nixcdn.com/singer/avatar/2020/08/14/5/1/1/9/1597380071736_600.jpg",
   },
   {
-    id: 3,
-
     name: "Muộn Rồi Mà Sao Còn",
     singer: "Sơn Tùng M-TP",
     path: "https://c1-ex-swe.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=tD-Ln6qGqkdH659AeuHsjQ&e=1638782546",
@@ -40,8 +36,6 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/04/29/9/1/f/8/1619691182261.jpg",
   },
   {
-    id: 4,
-
     name: "Thức Giấc",
     singer: "Da LAB",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1018/ThucGiac-DaLAB-7048212.mp3?st=1LcQhTisk8WrOQuzK4p86Q&e=1638782708",
@@ -49,17 +43,13 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/07/14/8/c/f/9/1626231010810.jpg",
   },
   {
-    id: 5,
-
-    name: "Độ Tộc 2",
-    singer: "Masew, Độ Mixi, Phúc Du, Pháo",
-    path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/DoToc2-MasewDoMixiPhucDuPhao-7064730.mp3?st=ehahZN3-iX9xYdBFgDgGcg&e=1638782785",
+    name: "Chúc Tết Mọi Nhà",
+    singer: "Hồ Ngọc Hà, Noo Phước Thịnh",
+    path: "https://aredir.nixcdn.com/NhacCuaTui934/ChucTetMoiNha-HoNgocHaNooPhuocThinh-4721729.mp3?st=VXl4YNXiyC3p4AaDj0ei-A&e=1643648475",
     image:
-      "https://avatar-nct.nixcdn.com/song/2021/08/10/b/2/e/0/1628579601228.jpg",
+      "https://avatar-ex-swe.nixcdn.com/singer/avatar/2020/08/14/5/1/1/9/1597380071736.jpg",
   },
   {
-    id: 6,
-
     name: "Chúng Ta Sau Này",
     singer: "T.R.I",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1010/ChungTaSauNay-TRI-6929586.mp3?st=l56Wr1fLE9fMnFehhpo5xg&e=1638782875",
@@ -67,8 +57,6 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/01/27/5/2/2/b/1611738358661.jpg",
   },
   {
-    id: 7,
-
     name: "Dịu Dàng Em Đến",
     singer: "ERIK, NinjaZ",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1021/DiuDangEmDen-ERIKNinjaZ-7078877.mp3?st=QmjyqbnGv3jClPKm4oA1YQ&e=1638782938",
@@ -76,8 +64,6 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/08/30/2/1/a/e/1630307726211.jpg",
   },
   {
-    id: 8,
-
     name: "Hương",
     singer: "Văn Mai Hương, Negav",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1010/Huong-VanMaiHuongNegav-6927340.mp3?st=PvHOWlRnF6TymvggYGding&e=1638783027",
@@ -85,8 +71,6 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/01/22/9/f/2/1/1611280898757.jpg",
   },
   {
-    id: 9,
-
     name: "Miên Man",
     singer: "DUTZUX",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1024/MienMan-DUTZUX-7120884.mp3?st=yTOFq5aH8FGEvbm-_n_KTA&e=1638783090",
@@ -94,9 +78,7 @@ const songs = [
       "https://avatar-nct.nixcdn.com/song/2021/11/19/6/d/9/1/1637320885751.jpg",
   },
   {
-    id: 10,
-
-    name: "có hẹn với thanh xuân",
+    name: "Có Hẹn Với Thanh Xuân",
     singer: "MONSTAR",
     path: "https://c1-ex-swe.nixcdn.com/NhacCuaTui1020/cohenvoithanhxuan-MONSTAR-7050201.mp3?st=PjrrnZ2dZ3ffA6R7dRrppQ&e=1638783161",
     image:
@@ -104,15 +86,39 @@ const songs = [
   },
 ];
 
-const firework = document.querySelector(".firework");
-const container = document.querySelector(".countdown-container");
-const footer = document.querySelector("footer");
-const countdownAudio = document.querySelector("#countdown-audio");
-const secondsElement = document.querySelector("#seconds");
-const items = document.querySelectorAll(".deadline-format h4");
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const PLAY_STORE_KEY = "playStore";
+
+const player = $(".player");
+const playlist = $(".playlist");
+const progress = $(".progress");
+const cd = $(".cd");
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const playBtn = $(".btn-toggle-play");
+const nextBtn = $(".btn-next");
+const prevBtn = $(".btn-prev");
+const repeatBtn = $(".btn-repeat");
+const randomBtn = $(".btn-random");
+
+let isPlaying = false;
+let isRandom = false;
+let isRepeat = false;
+
+let currentIndex = 0;
+
+const firework = $(".firework");
+const container = $(".countdown-container");
+const footer = $("footer");
+const countdownAudio = $("#countdown-audio");
+const secondsElement = $("#seconds");
+const items = $$(".deadline-format h4");
 
 const timeNow = new Date().getTime();
-const futureDate = new Date(timeNow + 200000);
+const futureDate = new Date(timeNow + 40000);
 const year = futureDate.getFullYear();
 const month = months[futureDate.getMonth()];
 const date = futureDate.getDate();
@@ -146,10 +152,15 @@ const getTime = () => {
     item.innerHTML = formatValue(value[index]);
   });
 
+  if (time < 30 * 1000) {
+    audio.pause();
+  }
+
   if (time < 12 * 1000 && time > 0) {
     secondsElement.style = "animation: zoom-in-zoom-out 1s ease infinite;";
     countdownAudio.play();
   }
+
   if (time < 1 * 1000) {
     clearInterval(countdown);
 
@@ -170,27 +181,6 @@ const addScript = (src) => {
 let countdown = setInterval(getTime, 1000);
 
 getTime();
-
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
-const PLAY_STORE_KEY = "playStore";
-
-const player = $(".player");
-const playlist = $(".playlist");
-const progress = $(".progress");
-const cd = $(".cd");
-const heading = $("header h2");
-const cdThumb = $(".cd-thumb");
-const audio = $("#audio");
-const playBtn = $(".btn-toggle-play");
-const nextBtn = $(".btn-next");
-const prevBtn = $(".btn-prev");
-const repeatBtn = $(".btn-repeat");
-const randomBtn = $(".btn-random");
-
-let isPlaying = false;
-let currentIndex = 0;
 
 const cdWidth = cd.offsetWidth;
 const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
@@ -242,8 +232,32 @@ prevBtn.addEventListener("click", () => {
   audio.play();
 });
 
+randomBtn.addEventListener("click", () => {
+  isRandom = !isRandom;
+  randomBtn.classList.toggle("active", isRandom);
+
+  isRepeat = false;
+  repeatBtn.classList.toggle("active", isRepeat);
+});
+
+repeatBtn.addEventListener("click", () => {
+  isRepeat = !isRepeat;
+  repeatBtn.classList.toggle("active", isRepeat);
+
+  isRandom = false;
+  randomBtn.classList.toggle("active", isRandom);
+});
+
 audio.addEventListener("ended", () => {
-  nextSong();
+  if (isRepeat) {
+    audio.play();
+  } else {
+    nextSong();
+  }
+
+  if (isRandom) {
+    randomSong();
+  }
   audio.play();
 });
 
@@ -252,6 +266,15 @@ function loadCurrentSong() {
   cdThumb.style.backgroundImage = `url("${songs[currentIndex].image}")`;
   audio.src = songs[currentIndex].path;
 }
+
+// function loadConfig() {
+//   currentIndex = config.currentIndex;
+//   isRandom = config.isRandom;
+//   isRepeat = config.isRepeat;
+
+//   repeatBtn.classList.toggle("active", isRepeat);
+//   randomBtn.classList.toggle("active", isRandom);
+// }
 
 function nextSong() {
   currentIndex++;
@@ -266,6 +289,16 @@ function prevSong() {
     currentIndex = songs.length;
   }
   currentIndex--;
+  loadCurrentSong();
+}
+
+function randomSong() {
+  let newIndex;
+  do {
+    newIndex = Math.floor(Math.random() * songs.length);
+  } while (newIndex === currentIndex);
+
+  currentIndex = newIndex;
   loadCurrentSong();
 }
 
